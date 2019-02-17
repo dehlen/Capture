d9a3096cdc439c010fd51e61499a73d05a53fd62
Ehlen, David <david.ehlen@dm.de>
2019-02-18 00:25:06 +0100

    remove comments

--- Annie/Utilities/Sugar.swift
+++ Annie/Utilities/Sugar.swift
@@ -1,9 +1,5 @@
 import Foundation
 
-//
-// The idea is borrowed from https://github.com/devxoul/Then
-//
-
 infix operator ≈
 infix operator …
 
@@ -19,24 +15,12 @@ public func with<T>(_ value: T, _ body: (inout T) throws -> Void) rethrows -> T 
 }
 
 public func ≈<T>(value: T, body: (inout T) throws -> Void) rethrows -> T {
-    // swiftlint:disable:previous identifier_name
     return try with(value, body)
 }
 @discardableResult
 public func …<T: AnyObject>(obj: T, body: (T) throws -> Void) rethrows -> T {
-    // swiftlint:disable:previous identifier_name
     return try with(obj, body)
 }
 
-/**
- Substitute for `Void` in `static let initializeOnce: Void = {...}`.
-
- `Void` breaks LLDB interactions when `initializeOnce` is visible as below:
-
- (lldb) p tableView.estimatedRowHeight
- error: Couldn't materialize: couldn't get the value of initializeOnce: extracting data from value failed
- error: errored out in DoExecute, couldn't PrepareToExecuteJITExpression
- */
 public typealias Ignored = Int
-
 public typealias UnusedKVOValue = AnyObject?
