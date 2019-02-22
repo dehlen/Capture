beaaa6d0f508cde377c64f552dd9313d44da0ab2
Ehlen, David <david.ehlen@dm.de>
2019-02-22 14:28:49 +0100

    fix collection cell layout to scale images correctly

--- Annie/WindowList/View/CollectionViewItem.swift
+++ Annie/WindowList/View/CollectionViewItem.swift
@@ -5,6 +5,11 @@ class CollectionViewItem: NSCollectionViewItem {
     @IBOutlet weak var titleLabel: NSTextField!
     @IBOutlet weak var appIconImageView: NSImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.appIconImageView.alphaValue = 0.5
+    }
+
     override var isSelected: Bool {
         willSet {
             view.layer?.backgroundColor = newValue ? NSColor.blue.cgColor : NSColor.clear.cgColor
