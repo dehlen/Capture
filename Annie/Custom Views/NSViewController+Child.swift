26785dda38ea92f9b9d024df76ffca39ad53a785
Ehlen, David <david.ehlen@dm.de>
2019-02-23 17:02:00 +0100

    remove unnecessary newlines and already fixed warning

--- Annie/Custom Views/NSViewController+Child.swift
+++ Annie/Custom Views/NSViewController+Child.swift
@@ -9,24 +9,26 @@ extension NSViewController {
 
     public func embed(_ vc: NSViewController, container: NSView? = nil) {
         addChild(vc)
-        #warning("views are not resizable")
+
         let targetView = container ?? view
         var frame = CGRect(x: 0, y: 0, width: targetView.frame.width, height: targetView.frame.height)
+
         if frame.size.width == 0 {
             frame.size.width = CGFloat.leastNormalMagnitude
         }
         if frame.size.height == 0 {
             frame.size.height = CGFloat.leastNormalMagnitude
         }
+
         vc.view.frame = frame
         targetView.addSubView(vc.view, animated: true)
+
         vc.view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             vc.view.leadingAnchor.constraint(equalTo: targetView.leadingAnchor),
             vc.view.trailingAnchor.constraint(equalTo: targetView.trailingAnchor),
             vc.view.topAnchor.constraint(equalTo: targetView.topAnchor),
             vc.view.bottomAnchor.constraint(equalTo: targetView.bottomAnchor),
-
         ])
     }
 
