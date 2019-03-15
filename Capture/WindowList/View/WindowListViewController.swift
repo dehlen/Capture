56f038a23f0709dcec0636442364688c3d40c143
Ehlen, David <david.ehlen@dm.de>
2019-03-15 12:02:16 +0100

    #24 add notification extensions back in

--- Capture/WindowList/View/WindowListViewController.swift
+++ Capture/WindowList/View/WindowListViewController.swift
@@ -1,5 +1,11 @@
 import AppKit
 
+extension NSNotification.Name {
+    static let shouldStopRecording = NSNotification.Name("on-should-stop-recording")
+    static let shouldStartRecording = NSNotification.Name("on-should-start-recording")
+    static let shouldStopSelection = NSNotification.Name("on-should-stop-selection")
+}
+
 class WindowListViewController: NSViewController {
     static let showContainer = "showContainer"
 
