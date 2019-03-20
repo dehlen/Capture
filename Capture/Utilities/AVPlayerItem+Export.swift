d9fd45c876e9263d4989bb00afa379b48ed0f04e
Ehlen, David <david.ehlen@dm.de>
2019-03-20 17:40:13 +0100

    refactoring

---
+++ Capture/Utilities/AVPlayerItem+Export.swift
@@ -0,0 +1,32 @@
+import AVKit
+import os
+
+extension AVPlayerItem {
+    typealias URLHandler = ((Result<URL>) -> Void)
+
+    func export(to destination: URL, then handler: @escaping URLHandler) {
+        let preset: String = Current.defaults[.movieQuality] ?? AVAssetExportPresetAppleM4V480pSD
+        let exportSession = AVAssetExportSession(asset: asset, presetName: preset)!
+        exportSession.outputFileType = AVFileType.m4v
+        exportSession.outputURL = destination
+
+        let startTime = reversePlaybackEndTime
+        let endTime = forwardPlaybackEndTime
+        let timeRange = CMTimeRangeFromTimeToTime(start: startTime, end: endTime)
+        exportSession.timeRange = timeRange
+
+        exportSession.exportAsynchronously {
+            os_log(.info, log: .videoPlayer, "Export Status changed %{public}i", exportSession.status.rawValue)
+            if let error = exportSession.error {
+                os_log(.error, log: .videoPlayer, "Export Status failed %{public}@", error.localizedDescription)
+            }
+            switch exportSession.status {
+            case .completed:
+                handler(.success(destination))
+            case .cancelled, .failed, .unknown:
+                handler(.failure(VideoPlayerError.exportFailed(reason: exportSession.error?.localizedDescription ?? "")))
+            default: ()
+            }
+        }
+    }
+}
--- Capture/VideoHandling/VideoPlayerViewController.swift
+++ Capture/VideoHandling/VideoPlayerViewController.swift
@@ -90,6 +90,46 @@ class VideoPlayerViewController: NSViewController {
         widthTextField.delegate = self
     }
 
+    /// Triggered via First Responder Chain
+    @IBAction func toggleTrimming(_ sender: Any) {
+        if playerView.canBeginTrimming {
+            playerView.beginTrimming(completionHandler: nil)
+        }
+    }
+}
+
+// MARK: - Export
+extension VideoPlayerViewController {
+    private func exportVideo(then handler: @escaping URLHandler) {
+        guard let playerItem = playerView.player?.currentItem else {
+            os_log(.info, log: .videoPlayer, "VideoPlayer current item is nil")
+            handler(.failure(VideoPlayerError.noCurrentItem))
+            return
+        }
+        guard let outputUrl = trimmedOutputUrl else {
+            os_log(.info, log: .videoPlayer, "Video file could not be found")
+            handler(.failure(VideoPlayerError.missingFile))
+            return
+        }
+
+        playerItem.export(to: outputUrl, then: handler)
+    }
+
+    @IBAction func export(_ sender: NSButton) {
+        sender.isEnabled = false
+        delegate?.requestLoadingIndicator()
+        exportVideo { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let videoUrl):
+                    self.handleSuccessfulExport(with: videoUrl)
+                case .failure(let error):
+                    self.handleExportFailure(with: error)
+                }
+            }
+        }
+    }
+
     private func convertVideoToGif(progressHandler: @escaping ProgressHandler, then handler: @escaping URLHandler) {
         guard let trimmedVideoOutputUrl = trimmedOutputUrl else { return }
         guard let exportFolderUrl = DirectoryHandler.exportFolder else { return }
@@ -111,96 +151,47 @@ class VideoPlayerViewController: NSViewController {
                                maximumHeight: height,
                                duration: duration,
                                progressHandler: progressHandler) { (result) in
-                switch result {
-                case .success:
-                    handler(.success(gifOutputUrl))
-                case .failure(let error):
-                    handler(.failure(error))
-                }
+                                switch result {
+                                case .success:
+                                    handler(.success(gifOutputUrl))
+                                case .failure(let error):
+                                    handler(.failure(error))
+                                }
             }
         }
     }
 
-    private func exportVideo(then handler: @escaping URLHandler) {
-        guard let playerItem = playerView.player?.currentItem else {
-            os_log(.info, log: .videoPlayer, "VideoPlayer current item is nil")
-            handler(.failure(VideoPlayerError.noCurrentItem))
-            return
+    private func handleSuccessfulExport(with videoUrl: URL) {
+        if Current.defaults[.saveVideo] == true {
+            DirectoryHandler.copy(from: videoUrl)
         }
-        guard let outputUrl = trimmedOutputUrl else {
-            os_log(.info, log: .videoPlayer, "Video file could not be found")
-            handler(.failure(VideoPlayerError.missingFile))
-            return
-        }
-
-        let preset: String = Current.defaults[.movieQuality] ?? AVAssetExportPresetAppleM4V480pSD
-        let exportSession = AVAssetExportSession(asset: playerItem.asset, presetName: preset)!
-        exportSession.outputFileType = AVFileType.m4v
-        exportSession.outputURL = outputUrl
-
-        let startTime = playerItem.reversePlaybackEndTime
-        let endTime = playerItem.forwardPlaybackEndTime
-        let timeRange = CMTimeRangeFromTimeToTime(start: startTime, end: endTime)
-        exportSession.timeRange = timeRange
-
-        exportSession.exportAsynchronously {
-            os_log(.info, log: .videoPlayer, "Export Status changed %{public}i", exportSession.status.rawValue)
-            if let error = exportSession.error {
-                os_log(.error, log: .videoPlayer, "Export Status failed %{public}@", error.localizedDescription)
-            }
-            switch exportSession.status {
-            case .completed:
-                handler(.success(outputUrl))
-            case .cancelled, .failed, .unknown:
-                handler(.failure(VideoPlayerError.exportFailed(reason: exportSession.error?.localizedDescription ?? "")))
-            default: ()
-            }
-        }
-    }
-}
-
-extension VideoPlayerViewController {
-    /// Triggered via First Responder Chain
-    @IBAction func toggleTrimming(_ sender: Any) {
-        if playerView.canBeginTrimming {
-            playerView.beginTrimming(completionHandler: nil)
-        }
-    }
-
-    @IBAction func export(_ sender: NSButton) {
-        sender.isEnabled = false
-        delegate?.requestLoadingIndicator()
-        exportVideo { result in
-            switch result {
-            case .success(let videoUrl):
-                if Current.defaults[.saveVideo] == true {
-                    DirectoryHandler.copy(from: videoUrl)
-                }
-                self.convertVideoToGif(progressHandler: { (progress) in
-                    self.delegate?.exportProgressDidChange(progress: progress)
-                }, then: { convertResult in
-                    DispatchQueue.main.async {
-                        sender.isEnabled = true
-                        switch convertResult {
-                        case .success(let gifOutputUrl):
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                self.delegate?.dismissLoadingIndicator()
-                                self.delegate?.requestReplace(new: FinishViewController.create(state: .success(gifOutputUrl)))
-                            }
-                        case .failure(let error):
-                            let errorMessage = ErrorMessageProvider.string(for: NSError.create(from: error))
-                            self.delegate?.requestReplace(new: FinishViewController.create(state: .failure(errorMessage)))
-                        }
-                    }
-                })
+        self.convertVideoToGif(progressHandler: { (progress) in
+            self.delegate?.exportProgressDidChange(progress: progress)
+        }, then: { convertResult in
+            switch convertResult {
+            case .success(let gifOutputUrl):
+                self.handleSuccesfulGifExport(with: gifOutputUrl)
             case .failure(let error):
-                DispatchQueue.main.async {
-                    sender.isEnabled = true
-                    let errorMessage = ErrorMessageProvider.string(for: error)
-                    self.delegate?.requestReplace(new: FinishViewController.create(state: .failure(errorMessage)))
-                }
+                self.handleGifExportFailure(with: error)
             }
+        })
+    }
+
+
+    private func handleSuccesfulGifExport(with gifOutputUrl: URL) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.delegate?.requestReplace(new: FinishViewController.create(state: .success(gifOutputUrl)))
         }
+    }
+
+    private func handleGifExportFailure(with error: Error) {
+        let errorMessage = ErrorMessageProvider.string(for: NSError.create(from: error))
+        self.delegate?.requestReplace(new: FinishViewController.create(state: .failure(errorMessage)))
+    }
+
+    private func handleExportFailure(with error: Error) {
+        let errorMessage = ErrorMessageProvider.string(for: error)
+        self.delegate?.requestReplace(new: FinishViewController.create(state: .failure(errorMessage)))
     }
 }
 
